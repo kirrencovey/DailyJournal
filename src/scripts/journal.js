e285ec1663793f320d5d26df22de7695e51d8ece
Kirren Covey <kirrencovey@gmail.com>
2019-01-16 08:57:33 -0600

    added journal styles

---
+++ src/scripts/journal.js
@@ -0,0 +1,22 @@
+let allJournalEntries = [];
+
+
+const objectsJournalEntry = {
+    journalDate: "11Jan18",
+    conceptsCovered: "JS Object Basics",
+    journalEntry: "Learned what an object is and how it's structured. Setting and accessing values using dot notation and square bracket notation.",
+    mood: "happy"
+};
+
+const functionsJournalEntry = {
+    journalDate: "14Jan18",
+    conceptsCovered: "JS Function Practice",
+    journalEntry: "Did many practice exercises with functions. Pure and impure functions. ChickenMonkey.",
+    mood: "I've got this!"
+};
+
+allJournalEntries.push(objectsJournalEntry);
+allJournalEntries.push(functionsJournalEntry);
+
+
+console.log(allJournalEntries);
